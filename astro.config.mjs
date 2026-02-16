6ac1a120948632b4fd042a4e39bca24c1e609126
tcwang-acrocyte <adamw0224@gmail.com>
2026-02-16 20:31:52 +0800

    add mini experiment (finding passion)

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,56 +17,60 @@ export default defineConfig({
 					label: 'English',
 				},
 			},
-			
+
 			sidebar: [
 				{
 					label: 'SPIRE Five Aspects in Life',
 					translations: { 'zh-TW': 'SPIRE模型 生活五大面向' },
 					items: [
-						{ label: 'Spirit', slug: 'five_aspect', translations: { 'zh-TW': 'Spirit 靈性' } },
-						{ label: 'Physical', slug: 'five_aspect', translations: { 'zh-TW': 'Physical 身體' } },
-						{ label: 'Intellectual', slug: 'five_aspect', translations: { 'zh-TW': 'Intellectual 智識' } },
-						{ label: 'Relational', slug: 'five_aspect', translations: { 'zh-TW': 'Relational 關係' } },
-						{ label: 'Emotional', slug: 'five_aspect', translations: { 'zh-TW': 'Emotional 情緒' } },
+						{ label: 'Spiritual', slug: 'five_aspect', translations: { 'zh-TW': 'Spiritual 靈性' } },
+						{ label: 'Physical', slug: 'physical', translations: { 'zh-TW': 'Physical 身體' } },
+						{ label: 'Intellectual', slug: 'intellectual', translations: { 'zh-TW': 'Intellectual 智識' } },
+						{ label: 'Relational', slug: 'relational', translations: { 'zh-TW': 'Relational 關係' } },
+						{ label: 'Emotional', slug: 'emotional', translations: { 'zh-TW': 'Emotional 情緒' } },
 					],
 				},
 				{
 					label: 'Spirit',
 					translations: { 'zh-TW': 'Spiritual 靈性' },
 					items: [
-						{ label: 'Purposes', slug: 'five_aspect', translations: { 'zh-TW': '目標' } },
-						{ label: 'Spirit Item1', slug: 'five_aspect', translations: { 'zh-TW': '靈性項目1' } },
+						{ label: 'Overview', slug: 'spiritual', translations: { 'zh-TW': '總覽' } },
+						{ label: 'Finding Passions', slug: 'reference/spiritual/finding_passions', translations: { 'zh-TW': '尋找熱情' } },
 					],
 				},
 				{
 					label: 'Physical',
 					translations: { 'zh-TW': 'Physical 身體' },
 					items: [
-						{ label: 'Sleep', slug: 'five_aspect', translations: { 'zh-TW': '睡眠' } },
-						{ label: 'Eat', slug: 'five_aspect', translations: { 'zh-TW': '飲食' } },
-						{ label: 'Exercise', slug: 'five_aspect', translations: { 'zh-TW': '運動' } },
+						{ label: 'Overview', slug: 'physical', translations: { 'zh-TW': '總覽' } },
+						{ label: 'Sleep', slug: 'reference/physical/sleep', translations: { 'zh-TW': '睡眠' } },
+						{ label: 'Eat', slug: 'reference/physical/eat', translations: { 'zh-TW': '飲食' } },
+						{ label: 'Exercise', slug: 'reference/physical/exercise', translations: { 'zh-TW': '運動' } },
 					],
 				},
 				{
 					label: 'Intellectual',
-					translations: { 'zh-TW': 'Intellecutal 智識' },
+					translations: { 'zh-TW': 'Intellectual 智識' },
 					items: [
-						{ label: 'Purposes', slug: 'five_aspect', translations: { 'zh-TW': '目標' } },
-						{ label: 'Cognitive', slug: 'five_aspect', translations: { 'zh-TW': '認知' } },
+						{ label: 'Overview', slug: 'intellectual', translations: { 'zh-TW': '總覽' } },
+						{ label: 'Purposes', slug: 'reference/intellectual/purposes', translations: { 'zh-TW': '目標' } },
+						{ label: 'Cognitive', slug: 'reference/intellectual/cognitive', translations: { 'zh-TW': '認知' } },
 					],
 				},
 				{
 					label: 'Relational',
 					translations: { 'zh-TW': 'Relational 關係' },
 					items: [
-						{ label: 'Friends', slug: 'five_aspect', translations: { 'zh-TW': '朋友' } },
+						{ label: 'Overview', slug: 'relational', translations: { 'zh-TW': '總覽' } },
+						{ label: 'Friends', slug: 'reference/relational/friends', translations: { 'zh-TW': '朋友' } },
 					],
 				},
 				{
 					label: 'Emotional',
 					translations: { 'zh-TW': 'Emotional 情緒' },
 					items: [
-						{ label: 'Comparision', slug: 'five_aspect', translations: { 'zh-TW': '比較' } },
+						{ label: 'Overview', slug: 'emotional', translations: { 'zh-TW': '總覽' } },
+						{ label: 'Comparison', slug: 'reference/emotional/comparison', translations: { 'zh-TW': '比較' } },
 					],
 				},
 				{
